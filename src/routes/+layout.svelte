c4c5ff25bbffdc91fab9ae2d88b59243778a5702
degwinthegreat <the.colledge.dropout@gmail.com>
2023-11-03 13:11:47 +0900

    headerを置いてみた

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,54 +1,34 @@
 <script>
 	import '../app.postcss';
-	import Header from './Header.svelte';
-	import './styles.css';
+	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton'
 </script>
 
-<div class="app">
-	<Header />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			Tech Tech App
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
 
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
 	<main>
 		<slot />
 	</main>
 
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
 	<footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
-</div>
+	<!-- (footer) -->
+</AppShell>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>

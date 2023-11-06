0b0e4c7fc64cacc677f69bf01e7e61631a78dbbb
degwinthegreat <the.colledge.dropout@gmail.com>
2023-11-06 22:36:01 +0900

    TOPページをそれっぽくした

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,20 @@
 <script>
 	import '../app.postcss';
-	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton'
+	import { AppBar, AppShell, Avatar, LightSwitch } from '@skeletonlabs/skeleton'
+	import tamamushiIcon from '$lib/images/tamamushi_icon.jpg'
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">
 		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-			<svelte:fragment slot="lead">(icon)</svelte:fragment>
-			Tech Tech App
+			<svelte:fragment slot="lead">Tech Tech App</svelte:fragment>
 			<svelte:fragment slot="trail">
+				<Avatar
+					src={tamamushiIcon}
+					border="border-4 border-surface-300-600-token hover:!border-primary-500"
+					cursor="cursor-pointer"
+					width="w-12"
+				/>
 				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
@@ -25,7 +31,6 @@
 	<!-- ---- / ---- -->
 	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 	<!-- (footer) -->
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,14 @@
-
-<svelte:head>
-	<title>Tech Tech App</title>
-	<meta name="description" content="Tech Tech App" />
-</svelte:head>
-
-<section>
-	<h1 class="h1">
-		hoge
-	</h1>
-
-	<h2 class="h2">
-		fuga
-	</h2>
-</section>
-
-<style>
-</style>
+<div class="relative isolate px-6 pt-14 lg:px-8">
+	<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+		<div class="text-center">
+			<h1 class="h1">
+				<span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Tech Tech Kochiの自己紹介を残すためのアプリです</span>
+			</h1>
+			<p class="mt-6 text-lg leading-8">登録しないと見れないようになってるから登録してみてね！</p>
+			<div class="mt-10 flex items-center justify-center gap-x-6">
+				<a href="#" class="btn variant-filled">ユーザー登録</a>
+				<a href="#" class="btn variant-soft">ログイン</a>
+			</div>
+		</div>
+	</div>
+</div>

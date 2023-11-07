33afdad839f065ae7e6f5e8f943123ba5934c4e8
degwinthegreat <the.colledge.dropout@gmail.com>
2023-11-08 08:50:03 +0900

    デバックログ仕込む

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,17 +6,40 @@ import GitHub from "@auth/core/providers/github"
 import { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 export const auth = SvelteKitAuth({
-   session: {
-    strategy: 'jwt',
-  },
   trustHost: true,
   secret: AUTH_SECRET,
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-  pages:{
-    signIn: '/sign_in',
-  }
-})
+  callbacks: {
+    jwt: async ({token, user, account, profile}) => {
+                // 注意: トークンをログ出力してはダメです。
+                console.log('in jwt', {user, token, account, profile})
 
+                if (user) {
+                    token.user = user;
+                    const u = user as any
+                    token.role = u.role;
+                }
+                if (account) {
+                    token.accessToken = account.access_token
+                }
+                return token;
+            },
+    session: ({session, token}) => {
+      console.log("in session", {session, token});
+      token.accessToken
+      return {
+        ...session,
+        user: {
+            ...session.user,
+            role: token.role,
+        },
+      }
+    }
+  },
+  pages:{
+      signIn: '/sign_in',
+    },
+})
 const protect=(async ({ event, resolve })=>{
   const protectedPages=["/users"];
   if(protectedPages.some(s=>event.url.pathname.startsWith(s))){

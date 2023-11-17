0b34fd9b2c6d6852c8edf31c044604d010670d29
degwinthegreat <the.colledge.dropout@gmail.com>
2023-11-17 13:54:30 +0900

    一旦実装できてない認証方法は非表示にした

--- src/routes/sign_in/+page.svelte
+++ src/routes/sign_in/+page.svelte
@@ -6,22 +6,22 @@
 {#if $page.data.session?.user}
   <button class="btn variant-filled-error" on:click={signOut}>ログアウト</button>
 {:else}
-  <h2 class="h1 text-center">パスワードでログイン</h2>
+  <!-- <h2 class="h1 text-center">パスワードでログイン</h2>
   <div class="logo-cloud bg-initial grid-cols-1 lg:!grid-cols-1 gap-1">
     <a class="logo-item" href="/#">
       <span><i class="fa-solid fa-key"></i></span>
       <span>パスワード</span>
     </a>
-  </div>
+  </div> -->
   <h2 class="h1 text-center">SNSでログイン</h2>
-  <div class="logo-cloud bg-initial grid-cols-1 lg:!grid-cols-2 gap-1">
+  <div class="logo-cloud bg-initial grid-cols-1 lg:!grid-cols-1 gap-1">
     <button class="logo-item" on:click={() => signIn('github', { callbackUrl: '/users' })}>
       <span><i class="fa-brands fa-github"></i></span>
       <span>GitHub</span>
     </button>
-    <button class="logo-item" on:click={() => signIn('slack', { callbackUrl: '/users' })}>
+    <!-- <button class="logo-item" on:click={() => signIn('slack', { callbackUrl: '/users' })}>
       <span><i class="fa-brands fa-slack"></i></span>
       <span>Slack</span>
-    </button>
+    </button> -->
   </div>
 {/if}
